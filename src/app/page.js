092e4940cccba2affbb30af75d3b42e6de3a4dff
unknown <iampankaj852@gmail.com>
2023-07-05 18:09:15 +0530

    2nd section improved

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="container flex md:w-10/12 sm:w-11/12 w-full md:flex-row sm:flex-col flex-col mx-auto md:mt-24 sm:mt-28 mt-24 border lg:px-4 md:px-3 sm:px-2 px-2 py-4 gap-y-8">
         <GetStartedSection />
       </div>
-      <div className="border w-full my-20 py-8 bg-orange-300 dark:bg-neutral-950">
+      <div className="border w-full mt-20 py-8 bg-orange-50 to-50% dark:bg-neutral-950">
         <div className="container md:w-10/12 sm:w-11/12 mx-auto flex w-full border lg:px-4 md:px-3 sm:px-2 px-2 py-4">
           <ExamUpdatesSection />
         </div>
