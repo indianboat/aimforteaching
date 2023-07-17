d8331ff17364bf05a5bb8452893aa20cb3e30493
pankaj <iampankaj852@gmail.com>
2023-07-17 18:26:17 +0530

    added some link in drawer

--- src/app/components/CollapseButton.jsx
+++ src/app/components/CollapseButton.jsx
@@ -14,33 +14,43 @@ const CollapseButton = () => {
 
   const examTypes = [
     {
+      index:0,
       examName:"All TET Exam"
     },
     {
+      index:1,
       examName:"KVS Exam"
     },
     {
+      index:2,
       examName:"DSSSB Exam"
     },
     {
+      index:3,
       examName:"NVS Exam"
     },
     {
+      index:4,
       examName:"Super TET Exam"
     },
     {
+      index:5,
       examName:"AWES Exam"
     },
     {
+      index:6,
       examName:"NDMC Exam"
     },
     {
+      index:7,
       examName:"SDMC Exam"
     },
     {
+      index:8,
       examName:"B.Ed Entrance"
     },
     {
+      index:9,
       examName:"EMRC Exam"
     }
   ]
@@ -72,7 +82,7 @@ const CollapseButton = () => {
                 {
                   examTypes.map((exam)=>{
                     return ( 
-                    <li className='flex w-full'>
+                    <li className='flex w-full' key={exam.index}>
                       <Link className='transition-all text-sm w-full font-medium hover:text-blue-800 dark:hover:text-orange-700 px-2 py-[1px]' href={"/"}>{exam.examName}</Link>
                     </li> 
                       )
