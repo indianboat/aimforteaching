2aca46fea3d5627253b508e23a6d422967dc6a27
pankaj <iampankaj852@gmail.com>
2023-07-19 14:04:49 +0530

    exam page added and some design

--- src/app/components/Breadcrum.jsx
+++ src/app/components/Breadcrum.jsx
@@ -9,9 +9,9 @@ const Breadcrumb = ({ links }) => {
       {links.map((link, index) => (
         <div key={link.path} className="flex items-center">
           {index > 0 && (
-            <RiArrowRightSLine className="text-gray-500"/>
+            <RiArrowRightSLine className="text-gray-500 dark:text-gray-200"/>
           )}
-            <Link href={link.path} className="text-gray-500 hover:text-blue-500">
+            <Link href={link.path} className="text-gray-500 hover:text-blue-600 dark:text-gray-200 dark:hover:text-orange-600">
             {link.label}
           </Link>
         </div>
